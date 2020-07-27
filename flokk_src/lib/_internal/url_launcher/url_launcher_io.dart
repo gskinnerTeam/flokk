e9e7d34b9353157fd8a11c1c93e4490418f12a0c
jwiese-ms <56444129+jwiese-ms@users.noreply.github.com>
2020-07-27 16:01:41 -0500

    Fixed URL Launching on Android/iOS

--- flokk_src/lib/_internal/url_launcher/url_launcher_io.dart
+++ flokk_src/lib/_internal/url_launcher/url_launcher_io.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flokk/_internal/log.dart';
 import 'package:universal_platform/universal_platform.dart';
+import 'package:url_launcher/url_launcher.dart' as urlLauncher;
 
 Future<bool> urlLauncherOpen(String url) async {
   ProcessResult result;
@@ -16,10 +17,15 @@ Future<bool> urlLauncherOpen(String url) async {
       result = await io.Process.run("start", [url], runInShell: true);
     } else if (UniversalPlatform.isMacOS) {
       result = await io.Process.run("open", [url]);
+    } else if (UniversalPlatform.isAndroid || UniversalPlatform.isIOS) {
+      if (await urlLauncher.canLaunch(url)) {
+        return await urlLauncher.launch(url);
+      }
+      ;
     }
   } on ProcessException catch (e) {
     Log.e(e?.message);
   }
-  ;
+
   return result?.exitCode == 0;
 }
