ef94181b51e4154ee472eb3ba4fdc5d9004fc1a0
Shawn <shawn@shawnblais.com>
2020-09-18 11:57:25 -0600

    Update Windows runner

--- flokk_src/windows/runner/window_configuration.cpp
+++
@@ -1,7 +0,0 @@
-#include "window_configuration.h"
-
-const wchar_t* kFlutterWindowTitle = L"Flokk Contacts";
-const unsigned int kFlutterWindowOriginX = 10;
-const unsigned int kFlutterWindowOriginY = 10;
-const unsigned int kFlutterWindowWidth = 800;
-const unsigned int kFlutterWindowHeight = 600;
